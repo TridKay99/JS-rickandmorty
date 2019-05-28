b19094b80a4667472b7a8ad5e99bf61f78bae52b
Tristan Kay <tridkay90@gmail.com>
2019-05-28 16:52:31 +1000

    completed character page

--- main.js
+++ main.js
@@ -17,17 +17,46 @@ fetch(`https://rickandmortyapi.com/api/character/${nums}`)
         let container = document.querySelector("#container");
 
         data.forEach(function(result) {
-        let p = document.createElement("p");
-        p.innerText = result.name;
-        console.log(result.name);
+        // Id number
+        let Id = document.createElement("p")
+        Id.innerText = result.id
 
+        // Name
+        let Name = document.createElement("p");
+        Name.innerText = result.name;
+
+        // Species
+        let Species = document.createElement("p")
+        Species.innerText = result.species
+
+        // Type e.g "Ants in his eyes human"
+        let Type = document.createElement("p")
+        Type.innerText = result.type
+        
+        // Creating 2 divs to place information inside
         let newDiv = document.createElement("Div")
+        let insideDiv = document.createElement("Div")
+
+        // Making all images a div
         newDiv.style.background = `url(${result.image})`
         newDiv.style.backgroundSize = "cover"
         newDiv.style.backgroundPosition = "center";
+        // creating a class for 'newDiv' for css
         newDiv.classList.add("card")
+
+        // creating a class for 'insideDiv' for css
+        insideDiv.classList.add("insideCard")
+        // Attaching the picture divs to the container.
         container.appendChild(newDiv);
-        newDiv.appendChild(p);
+
+        // Attaching another div to the inside of the picture divs
+        newDiv.appendChild(insideDiv);
+
+        // Attaching info to the insideDiv.
+        insideDiv.appendChild(Id);
+        insideDiv.appendChild(Name);
+        insideDiv.appendChild(Species);
+        insideDiv.appendChild(Type);
     });
   })}
 
