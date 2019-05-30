f8951c2dc8c70db039795d85e254309b5e586041
Tristan Kay <tridkay90@gmail.com>
2019-05-30 15:59:04 +1000

    finished spinning icon

---
+++ index.js
@@ -0,0 +1,6 @@
+let elementToChange = document.getElementsByTagName("body")[0];
+elementToChange.style.cursor = "url('raygun.png'), auto";
+
+// elementsToChange.style.cursor = "raygun.png";
+
+// body.appendChild
